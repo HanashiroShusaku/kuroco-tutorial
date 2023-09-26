298b35f94a1c72c34d12e1d95679b65510f2c1d4
ShusakuHanashiro <hanashiro@colrevo.co.jp>
2023-09-26 18:38:54 +0900

    環境変数をnuxt.config.tsで記述するテスト

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // nuxt devtoolsを有効化（デフォルト）
   devtools: { enabled: true },
+  // 拡張機能をインストールしているのでshimはオフ
   typescript: {
     shim: false,
   },
+  // 環境変数の設定
+  runtimeConfig: {
+    baseUrl: 'https://colrevo-hana-test.g.kuroco.app',
+    public: {
+      baseUrl: process.env.BASE_URL,
+    }
+  },
+  // SSR無効化
+  ssr: false,
+  // グローバルのヘッダ
+  app: {
+    head: {
+      title: 'colrevo-hana-test',
+      htmlAttrs: {
+        lang: 'ja',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    }
+  }
 });
