577048894a751217a389ee1c105502345e242928
ShusakuHanashiro <hanashiro@colrevo.co.jp>
2023-09-27 16:04:16 +0900

    プレビューページの作成
    https://kuroco.app/ja/docs/tutorials/integrate-preview-page/

---
+++ pages/tutorials/integrate-preview-page/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 class="title">{{ data.details.subject }}</h1>
+    <div class="post" v-html="data.details.contents"></div>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+const config = useRuntimeConfig();
+const url = `${config.public.baseUrl}/rcms-api/5/news/preview`;
+
+const { data, pending, error, refresh } = await useFetch(url,{
+  query: {
+    preview_token: route.query.preview_token,
+  },
+});
+</script>
